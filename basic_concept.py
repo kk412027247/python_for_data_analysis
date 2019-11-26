43f38b19d7d0ed70ed99d260ffec8005025df963
TMD <kk412027247@gmail.com>
2019-11-27 01:08:28 +0800

    extractor draw graph function

--- basic_concept.py
+++ basic_concept.py
@@ -2,6 +2,7 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
+
 # def random_score(mu=90, sigma=15):
 #     score = np.random.normal(mu, sigma, 1000)
 #     count, bins, ignored = plt.hist(score, 100, density=True)
@@ -33,27 +34,37 @@ import matplotlib.pyplot as plt
 #
 # plt.show()
 #
+
+
+def draw_graph(score):
+    count, bins, ignored = plt.hist(score, 100, density=True)
+    std = np.std(score)
+    mean = np.mean(score)
+    plt.plot(bins, 1 / (std * np.sqrt(2 * np.pi)) * np.exp(- (bins - mean) ** 2 / (2 * std ** 2)), linewidth=2,
+             color='r')
+    plt.show()
+
+
 chinese_score = np.loadtxt('./chinese_score.csv', delimiter=',')
 chinese_score = np.around(chinese_score, decimals=1)
-plt.hist(chinese_score, 100, density=True)
-plt.show()
+draw_graph(chinese_score)
 
 math_score = np.loadtxt('./math_score.csv', delimiter=',')
 math_score = np.around(math_score, decimals=1)
-plt.hist(math_score, 100, density=True)
-plt.show()
+draw_graph(math_score)
 
 english_score = np.loadtxt('./english_score.csv', delimiter=',')
 english_score = np.around(english_score, decimals=1)
-plt.hist(english_score, 100, density=True)
-plt.show()
+draw_graph(english_score)
 
 comprehensive_score = np.loadtxt('./comprehensive_score.csv', delimiter=',')
 comprehensive_score = np.around(comprehensive_score, decimals=1)
-plt.hist(comprehensive_score, 100, density=True)
-plt.show()
+draw_graph(comprehensive_score)
 
 total_score = chinese_score + math_score + english_score + comprehensive_score
+draw_graph(total_score)
 
-plt.hist(total_score, 100, density=True)
-plt.show()
+tom_chinese_score = 123.5
+tom_math_score = 98
+tom_english_score = 107
+
