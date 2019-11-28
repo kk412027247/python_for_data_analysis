ea095efe900afe0532c7dbe8c30a0d561b2a8488
TMD <kk412027247@gmail.com>
2019-11-29 00:40:56 +0800

    find position of score

--- basic_concept.py
+++ basic_concept.py
@@ -14,18 +14,22 @@ import matplotlib.pyplot as plt
 # random_score(200, 30)
 
 
-# score11 = np.around(score1, decimals=1)
-
+# score11 = np.around(score1, decimals=0)
+#
 # chinese_score = np.random.normal(90, 15, 1000)
+# chinese_score = np.around(chinese_score, decimals=0)
 # np.savetxt('chinese_score.csv', chinese_score, delimiter=',')
 #
 # math_score = np.random.normal(80, 20, 1000)
+# math_score = np.around(math_score, decimals=0)
 # np.savetxt('math_score.csv', math_score, delimiter=',')
 #
 # english_score = np.random.normal(85, 17, 1000)
+# english_score = np.around(english_score, decimals=0)
 # np.savetxt('english_score.csv', english_score, delimiter=',')
-
+#
 # comprehensive_score = np.random.normal(200, 30, 1000)
+# comprehensive_score = np.around(comprehensive_score, decimals=0)
 # np.savetxt('comprehensive_score.csv', comprehensive_score, delimiter=',')
 
 # sum = score1 + score2 + score3
@@ -34,15 +38,21 @@ import matplotlib.pyplot as plt
 #
 # plt.show()
 #
-
+#
 
 def draw_graph(score):
-    count, bins, ignored = plt.hist(score, 100, density=True)
+    count, bins, ignored = plt.hist(score, 70, density=True)
     std = np.std(score)
     mean = np.mean(score)
     plt.plot(bins, 1 / (std * np.sqrt(2 * np.pi)) * np.exp(- (bins - mean) ** 2 / (2 * std ** 2)), linewidth=2,
              color='r')
     plt.show()
+
+
+def find_position(score, score_list):
+    for (index, value) in enumerate(np.sort(score_list)):
+        if value > score:
+            return index - 1
 
 
 chinese_score = np.loadtxt('./chinese_score.csv', delimiter=',')
@@ -64,7 +74,21 @@ draw_graph(comprehensive_score)
 total_score = chinese_score + math_score + english_score + comprehensive_score
 draw_graph(total_score)
 
-tom_chinese_score = 123.5
+tom_chinese_score = 123
 tom_math_score = 98
 tom_english_score = 107
+tom_comprehensive_score = 224
+tom_total_score = tom_chinese_score + tom_math_score + tom_english_score + tom_comprehensive_score
+
+tom_chinese_score_position = find_position(tom_chinese_score, chinese_score)
+tom_math_score_position = find_position(tom_math_score, math_score)
+tom_english_score_position = find_position(tom_english_score, english_score)
+tom_comprehensive_score_position = find_position(tom_comprehensive_score, comprehensive_score)
+tom_tom_total_score = find_position(tom_total_score, total_score)
+
+print(tom_chinese_score_position)
+print(tom_math_score_position)
+print(tom_english_score_position)
+print(tom_comprehensive_score_position)
+print(tom_tom_total_score)
 
