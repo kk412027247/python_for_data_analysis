a3c7884b88992bd0eefd0f8aa4643e9967f2f03f
TMD <kk412027247@gmail.com>
2019-12-02 01:02:33 +0800

    calculate percentile \ variance \ standard deviation

--- basic_concept.py
+++ basic_concept.py
@@ -41,6 +41,7 @@ import matplotlib.pyplot as plt
 #
 
 def draw_graph(score):
+    return
     count, bins, ignored = plt.hist(score, 70, density=True)
     std = np.std(score)
     mean = np.mean(score)
@@ -50,7 +51,8 @@ def draw_graph(score):
 
 
 def find_position(score, score_list):
-    for (index, value) in enumerate(np.sort(score_list)):
+    ranking_list = np.unique(score_list)
+    for (index, value) in enumerate(np.sort(ranking_list)):
         if value > score:
             return index - 1
 
@@ -74,6 +76,18 @@ draw_graph(comprehensive_score)
 total_score = chinese_score + math_score + english_score + comprehensive_score
 draw_graph(total_score)
 
+total_score_median = np.median(total_score)
+# 中位数
+print('total_score_median', total_score_median)
+# 百分位数
+print('percentile', np.percentile(total_score, 50))
+# 方差
+# 在概率论和统计学中，一个随机变量的方差描述的是它的离散程度，也就是该变量离其期望值的距离。
+print('variance', np.var(total_score))
+# 标注差
+# 标准差（又称标准偏差、均方差，英语：Standard Deviation，缩写SD），数学符号σ（sigma），在概率统计中最常使用作为测量一组数值的离散程度之用。标准差定义：为方差开算术平方根，反映组内个体间的离散程度；标准差与期望值之比为标准离差率。
+print('standard deviation', np.std(total_score))
+
 tom_chinese_score = 123
 tom_math_score = 98
 tom_english_score = 107
@@ -86,9 +100,12 @@ tom_english_score_position = find_position(tom_english_score, english_score)
 tom_comprehensive_score_position = find_position(tom_comprehensive_score, comprehensive_score)
 tom_tom_total_score = find_position(tom_total_score, total_score)
 
-print(tom_chinese_score_position)
-print(tom_math_score_position)
-print(tom_english_score_position)
-print(tom_comprehensive_score_position)
-print(tom_tom_total_score)
+# print(tom_chinese_score_position)
+# print(tom_math_score_position)
+# print(tom_english_score_position)
+# print(tom_comprehensive_score_position)
+# print(tom_tom_total_score)
 
+
+# 标准分
+# 标准分，是一种由原始分推导出来的相对地位量数，它是用来说明原始分在所属的那批分数中 的相对位置的。考生在接受测验后，按照评分标准对其作答反应直接评出来的分数，叫原始分。
