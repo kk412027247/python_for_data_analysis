d834c43321cfd3479e2251835b168ac62bb7d3f2
tmd <kk412027247@gmail.com>
2019-12-02 19:53:11 +0800

    draw vertical line

--- basic_concept.py
+++ basic_concept.py
@@ -40,11 +40,39 @@ import matplotlib.pyplot as plt
 #
 #
 
-def draw_graph(score):
-    return
-    count, bins, ignored = plt.hist(score, 70, density=True)
-    std = np.std(score)
-    mean = np.mean(score)
+def show_basic_value(score_list):
+    total_score_median = np.median(score_list)
+    percentile_25 = np.percentile(score_list, 25)
+    percentile_75 = np.percentile(score_list, 75)
+    variance = np.var(score_list)
+    standard_deviation = np.std(score_list)
+    # 中位数
+    # 百分位数
+    # 方差
+    # 在概率论和统计学中，一个随机变量的方差描述的是它的离散程度，也就是该变量离其期望值的距离。
+    # 标注差
+    # 标准差（又称标准偏差、均方差，英语：Standard Deviation，缩写SD），数学符号σ（sigma），在概率统计中最常使用作为测量一组数值的离散程度之用。
+    # 标准差定义：为方差开算术平方根，反映组内个体间的离散程度；标准差与期望值之比为标准离差率。
+    print('中位数: ', total_score_median, ', 25%分位:', percentile_25, ', 75%分位:', percentile_75, ', 方差:', variance,
+          ', 标准差:', standard_deviation)
+    return percentile_25, total_score_median, percentile_75
+
+
+def draw_graph(score_list):
+    ranking_list = np.unique(score_list)
+    ranking_list_length = len(ranking_list)
+    count, bins, ignored = plt.hist(score_list, bins=ranking_list_length, density=True)
+    x_label = 'range: ' + str(ranking_list[0]) + ' ~ ' + str(ranking_list[-1])
+    print(x_label)
+    plt.xlabel(x_label)
+    plt.ylabel("proportion")
+    std = np.std(score_list)
+    mean = np.mean(score_list)
+    # 25% 50% 75% percentile value
+    percentile_25, total_score_median, percentile_75 =    show_basic_value(score_list)
+    plt.vlines(percentile_25, 0, np.exp(-(percentile_25 - mean) ** 2 /(2* std **2)) / (np.sqrt(2 * np.pi) * std), colors ="r", )
+    plt.vlines(total_score_median, 0, np.exp(-(total_score_median - mean) ** 2 /(2* std **2)) / (np.sqrt(2 * np.pi) * std), colors ="r", )
+    plt.vlines(percentile_75, 0, np.exp(-(percentile_75 - mean) ** 2 /(2* std **2)) / (np.sqrt(2 * np.pi) * std), colors ="r", )
     plt.plot(bins, 1 / (std * np.sqrt(2 * np.pi)) * np.exp(- (bins - mean) ** 2 / (2 * std ** 2)), linewidth=2,
              color='r')
     plt.show()
@@ -55,6 +83,8 @@ def find_position(score, score_list):
     for (index, value) in enumerate(np.sort(ranking_list)):
         if value > score:
             return index - 1
+
+
 
 
 chinese_score = np.loadtxt('./chinese_score.csv', delimiter=',')
@@ -76,18 +106,6 @@ draw_graph(comprehensive_score)
 total_score = chinese_score + math_score + english_score + comprehensive_score
 draw_graph(total_score)
 
-total_score_median = np.median(total_score)
-# 中位数
-print('total_score_median', total_score_median)
-# 百分位数
-print('percentile', np.percentile(total_score, 50))
-# 方差
-# 在概率论和统计学中，一个随机变量的方差描述的是它的离散程度，也就是该变量离其期望值的距离。
-print('variance', np.var(total_score))
-# 标注差
-# 标准差（又称标准偏差、均方差，英语：Standard Deviation，缩写SD），数学符号σ（sigma），在概率统计中最常使用作为测量一组数值的离散程度之用。标准差定义：为方差开算术平方根，反映组内个体间的离散程度；标准差与期望值之比为标准离差率。
-print('standard deviation', np.std(total_score))
-
 tom_chinese_score = 123
 tom_math_score = 98
 tom_english_score = 107
@@ -108,4 +126,5 @@ tom_tom_total_score = find_position(tom_total_score, total_score)
 
 
 # 标准分
-# 标准分，是一种由原始分推导出来的相对地位量数，它是用来说明原始分在所属的那批分数中 的相对位置的。考生在接受测验后，按照评分标准对其作答反应直接评出来的分数，叫原始分。
+# 标准分，是一种由原始分推导出来的相对地位量数，它是用来说明原始分在所属的那批分数中 的相对位置的。
+# 考生在接受测验后，按照评分标准对其作答反应直接评出来的分数，叫原始分。
