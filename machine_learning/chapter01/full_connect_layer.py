1b81fee866ef9550af1826f0b10fe3af7bea9e65
TMD <kk412027247@gmail.com>
2020-03-24 01:04:32 +0800

    finish mnist keras compile

--- machine_learning/chapter01/full_connect_layer.py
+++ machine_learning/chapter01/full_connect_layer.py
@@ -2,16 +2,37 @@ import numpy as np
 
 
 class FullyConnectLayer(object):
-    def __init__(self, num_inputs, layer_size, activation_fn):
+    def __init__(self, num_inputs, layer_size, activation_function, derivated_activation_function=None):
         super().__init__()
         self.W = np.random.standard_normal((num_inputs, layer_size))
         self.b = np.random.standard_normal(layer_size)
         self.size = layer_size
-        self.activation_fn = activation_fn
+
+        self.activation_function = activation_function
+        self.derivated_activation_function = derivated_activation_function
+        self.x, self.y = None, None
 
     def forward(self, x):
         z = np.dot(x, self.W) + self.b
-        return self.activation_fn(z)
+        self.y = self.activation_function(z)
+        self.x = x
+        return self.y
+
+    def backward(self, dL_dy):
+        dy_dz = self.derivated_activation_function(self.y)
+        dL_dz = (dL_dy * dy_dz)
+        dz_dw = self.x.T
+        dz_dx = self.W.T
+        dz_db = np.ones(dL_dy.shape[0])
+        self.dL_dW = np.dot(dz_dw, dL_dz)
+        self.dL_db = np.dot(dz_db, dL_dz)
+
+        dL_dx = np.dot(dL_dz, dz_dx)
+        return dL_dx
+
+    def optimize(self, epsilon):
+        self.W -= epsilon * self.dL_dW
+        self.b -= epsilon * self.dL_db
 
 
 np.random.seed(42)
@@ -19,9 +40,10 @@ np.random.seed(42)
 x1 = np.random.uniform(-1, 1, 2).reshape(1, 2)
 x2 = np.random.uniform(-1, 1, 2).reshape(1, 2)
 
-relu_fn = lambda y: np.maximum(y, 0)
+relu_function = lambda y: np.maximum(y, 0)
 
-layer = FullyConnectLayer(2, 3, relu_fn)
+layer = FullyConnectLayer(num_inputs=2, layer_size=3,
+                          activation_function=relu_function, derivated_activation_function=None)
 
 out1 = layer.forward(x1)
 out2 = layer.forward(x2)
---
+++ machine_learning/chapter02/mnist_keras.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+import os
+
+proxy = 'http://127.0.0.1:8123'
+os.environ['http_proxy'] = proxy
+os.environ['HTTP_PROXY'] = proxy
+os.environ['https_proxy'] = proxy
+os.environ['HTTPS_PROXY'] = proxy
+
+num_classes = 10
+img_rows, img_cols = 28, 28
+num_channels = 1
+input_channels = 1
+input_shape = (img_rows, img_cols, num_channels)
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# print(x_train[0])
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dense(num_classes, activation='softmax'))
+
+model.compile(optimizer='sgd', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=5, verbose=1, validation_data=(x_test, y_test))
+print(model.summary())
