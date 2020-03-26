a2aa5ef8367d79961fb5fca929b34067df8dcf05
TMD <kk412027247@gmail.com>
2020-03-27 00:50:00 +0800

    finish sample convolutional neural network

---
+++ machine_learning/chapter03/learn_convolutions.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
+
+num_classes = 10
+img_rows, img_cols = 28, 28
+num_channels = 1
+input_channels = 1
+input_shape = (img_rows, img_cols, num_channels)
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+
+
+model = Sequential()
+
+model.add(Conv2D(6, kernel_size=(5, 5), padding='same', activation='relu', input_shape=(28, 28, 1)))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(16, kernel_size=(5,5), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Flatten())
+model.add(Dense(120, activation='relu'))
+model.add(Dense(84, activation='relu'))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(optimizer='sgd', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+callbacks = [tf.keras.callbacks.TensorBoard('./logs_keras')]
+
+print(model.summary())
+
+model.fit(x_train, y_train, epochs=5, verbose=1, validation_data=(x_test, y_test), callbacks=callbacks)
+
+
